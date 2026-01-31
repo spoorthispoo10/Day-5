22ac891db53589ab336a4d6377df8cbeffe647e0
SPOORTHI <spoogowda56@gmail.com>
2026-01-31 14:43:43 +0530

    Add files via upload

---
+++ Class1.java
@@ -0,0 +1,20 @@
+class Animal{
+    void Sound(){
+        System.out.println("The sound of Dog?");
+    }
+}
+class Dog extends Animal{
+    void Bark(){
+        System.out.println("Dog barks bow bow!!!!");
+}
+
+    }
+    class class1{
+        public static void main(String[]args){
+            Dog dg=new Dog();
+            dg.Sound();
+            dg.Bark();
+        
+        }
+
+    }
